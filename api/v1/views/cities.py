0c38c19a313019a15a2819e69d529423ecd88436
daorejuela1 <daorejuela1@outlook.com>
2020-09-10 16:10:35 +0000

    added cities and places_reviews object

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -52,16 +52,20 @@ def del_city(city_id):
     return jsonify({})
 
 
-@app_views.route('/cities/', strict_slashes=False, methods=['POST'])
-def post_city():
+@app_views.route('/states/<state_id>/cities',
+                 strict_slashes=False, methods=['POST'])
+def post_city(state_id):
     """
     Create a new City object
     """
+    state_obj = storage.get(classes["State"], state_id)
+    if state_obj is None:
+        abort(404)
     data_json = request.get_json(force=True, silent=True)
     if (type(data_json) is not dict):
         abort(400, "Not a JSON")
     if "name" in data_json:
-        new_city = classes["City"](**data_json)
+        new_city = classes["City"](state_id=state_id, **data_json)
         storage.new(new_city)
         storage.save()
         return jsonify(new_city.to_dict()), 201
