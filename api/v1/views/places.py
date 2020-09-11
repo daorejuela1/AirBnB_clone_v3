23ed8ea41a6ba60b4e665209cf1c3ceac8c14d38
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-10 19:06:54 -0500

    Function that creates a new Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -52,14 +52,14 @@ def del_place(place_id):
     return jsonify({})
 
 
-@app_views.route('/places/<place_id>/reviews',
+@app_views.route('/cities/<city_id>/places',
                  strict_slashes=False, methods=['POST'])
-def post_place(place_id):
+def post_place(city_id):
     """
     Create a new Place object
     """
-    place_obj = storage.get(classes["Place"], place_id)
-    if place_obj is None:
+    city_obj = storage.get(classes["City"], city_id)
+    if city_obj is None:
         abort(404)
     data_json = request.get_json(force=True, silent=True)
     if (type(data_json) is not dict):
@@ -69,10 +69,10 @@ def post_place(place_id):
     user_obj = storage.get(classes["User"], data_json["user_id"])
     if user_obj is None:
         abort(404)
-    if "text" not in data_json:
-        abort(400, "Missing text")
+    if "name" not in data_json:
+        abort(400, "Missing name")
     else:
-        new_place = classes["Place"](place_id=place_id, **data_json)
+        new_place = classes["Place"](city_id=city_id, **data_json)
         storage.new(new_place)
         storage.save()
         return jsonify(new_place.to_dict()), 201
